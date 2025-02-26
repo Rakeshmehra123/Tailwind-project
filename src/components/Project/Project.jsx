b4379e0834b5bd970ac058733769d0f90041bb24
Rakeshmehra123 <merakesh61@gamil.com>
2025-02-26 23:27:24 +0530

    commit : update project

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,21 +1,16 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Project = () => {
   return (
     <div>
-      <div className="relative flex items-center space-x-4">
-        {/* <img
-          src="./src/assets/picture.svg"
-          alt="pic"
-          className='"w-90 h-90 object-cover absolute flex ml-200 mt-150 rounded-4xl'
-        ></img>*/}
-      </div>
-      <div className="">
-        <h1 className="text-white text-2xl font-md ml-50 mt-10 text-amber-100 flex">
+     
+      <div className="mx-auto">
+        <h1 className="text-white ml-20  text-3xl font-md  mt-30 text-amber-100 flex">
           Key logger
         </h1>
 
-        <p className=" text-white text-lg mt-5  pt-2 ml-50 text-amber-100">
+        <p className=" text-white  ml-20  text-lg mt-5 m-8 pt-2  text-amber-100">
           {" "}
           A Keylogger Project is a web page built using JavaScript HTML css and
           designed to monitor and record the keystrokes made on a computer or
@@ -26,20 +21,20 @@ const Project = () => {
       </div>
       <div>
         <br></br>
-        <a
-          href="https://keystrokelogger.netlify.app/"
+        <Link
+          to="https://keystrokelogger.netlify.app/"
           target="_blank"
-          className="px-12 py-3 mt-5 ml-50 rounded-2xl bg-gradient-to-r from-green-400 to-lime-600 text-amber-100 pointer"
+          className="px-12 mt-5 py-5  ml-20  rounded-2xl bg-gradient-to-r from-green-400 to-lime-600 text-amber-100 pointer"
         >
           KeyLogger
-        </a>
+        </Link>
       </div>
       <div className="">
-        <h1 className="text-white text-2xl font-md ml-50 mt-10 text-amber-100 flex">
+        <h1 className="text-white ml-20  text-3xl font-md  mt-30 text-amber-100 flex">
           Signature App
         </h1>
 
-        <p className=" text-white text-lg mt-5  pt-2 ml-50 text-amber-100">
+        <p className=" text-white  ml-20  text-lg mt-5 m-8 pt-2  text-amber-100">
           {" "}
           A Signature App built using JavaScript, HTML, and CSS allows users to
           draw and save their digital signature on a web page. This app would
@@ -50,13 +45,13 @@ const Project = () => {
       </div>
       <div>
         <br></br>
-        <a
-          href="https://signforauth.netlify.app/"
+        <Link
+          to="https://signforauth.netlify.app/"
           target="_blank"
-          className="px-12 py-3 mt-5 ml-50 rounded-2xl bg-gradient-to-r from-green-400 to-lime-600 text-amber-100 pointer"
+          className="px-12 mt-5 py-5  ml-20  rounded-2xl bg-gradient-to-r from-green-400 to-lime-600 text-amber-100 pointer"
         >
           Signature App
-        </a>
+        </Link>
       </div>
     </div>
   );
